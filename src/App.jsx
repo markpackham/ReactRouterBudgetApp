bdf90c364cf608f50c54ed58077f103c7fae351f
mark <markpackham1@gmail.com>
2023-03-07 13:58:18 +0000

    demo basic route
    https://youtu.be/ZsOgF18Xldw?t=255

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+      <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
