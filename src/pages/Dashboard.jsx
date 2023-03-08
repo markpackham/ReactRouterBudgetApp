83bd5dd87787ef03e5baf700a488e932fc8ed838
mark <markpackham1@gmail.com>
2023-03-08 13:53:15 +0000

    add Dashboard component

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+// React Router DOM imports
+import { useLoaderData } from "react-router-dom";
+
+//  Helper functions  
+import { fetchData } from "../helpers"
+
+// Loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName }
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData()
+
+  return (
+    <div>
+      <h1>{userName}</h1>
+      Dashboard
+    </div>
+  )
+}
+export default Dashboard
