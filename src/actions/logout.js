b479b1bfb83e3128e0f9db7bad05330ade53615b
mark <markpackham1@gmail.com>
2023-03-09 10:02:47 +0000

    add delete user action
    https://youtu.be/cy0vEow4KA4?t=1007

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,7 +1,14 @@
+// React Router DOM imports
 import { redirect } from "react-router-dom";
+
+// Helpers
+import { deleteItem } from "../helpers";
 
 export async function logoutAction() {
   // Delete user
+  deleteItem({
+    key: "userName",
+  });
 
   // Return redirect
   return redirect("/");
