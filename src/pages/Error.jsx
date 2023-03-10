b46c1dfd542b6bc9cb5c36d9b898093a9354fa61
mark <markpackham1@gmail.com>
2023-03-10 08:53:01 +0000

    allow user to return to previous page
    https://www.youtube.com/watch?v=PbgVdQvhtqU

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,18 +1,22 @@
-import { useRouteError, Link } from "react-router-dom"
+import { useRouteError, Link, useNavigate } from "react-router-dom"
 
 // Library
 import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
 
 const Error = () => {
   const error = useRouteError() 
-  
+  // lets us get user browser history so we can return them to previous page
+  const navigate = useNavigate()
 
   return (
     <div className="error">
       <h1>Oh dear we have a problem</h1>
       <p>{error.message || error.status}</p>
       <div className="flex-md">
-        <button className="btn btn--dark">
+        <button className="btn btn--dark"
+        // go back 1 page in your browser history
+        onClick={() => navigate(-1)}
+        >
           <ArrowUturnLeftIcon width={20} />
           <span>Go Back</span>
         </button>
