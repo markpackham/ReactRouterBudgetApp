fc95979e61a90a36eb42a3a86fe86ca81e1c03c1
mark <markpackham1@gmail.com>
2023-03-10 09:25:26 +0000

    finish off AddBudgetForm
    https://youtu.be/PbgVdQvhtqU?t=334

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,6 +1,27 @@
+import { Form } from "react-router-dom"
+
 const AddBudgetForm = () => {
   return (
-    <div>AddBudgetForm</div>
+    <div className="form-wrapper">
+        <h2 className="h3">
+            Create budget
+        </h2>
+        <Form method="post" className="grid-sm">
+        <div className="grid-xs">
+            <label htmlFor="newBudget">Budget Name</label>
+            <input type="text" name="newBudget" id="newBudget" placeholder="e.g Groceries" required />
+        </div>
+        <div className="grid-xs">
+            <label htmlFor="newBudgetAmount">Amount</label>
+            <input type="number" step="0.01"
+            name="newBudgetAmount" id="newBudgetAmount" placeholder="e.g £50"
+            required
+            // decimal mode is handy for mobiles since it offers the "." symbol when typing
+            inputMode="decimal"
+            />
+        </div>
+        </Form>
+    </div>
   )
 }
 
