dd41ece322c0bef09c2e0bddf9c2064d9ceb5b8d
mark <markpackham1@gmail.com>
2023-03-10 08:34:27 +0000

    supply either an error message or error status
    https://youtu.be/KeeYaNnWd1k?t=201

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,23 @@
+import { useRouteError, Link } from "react-router-dom"
+
+// Library
+import { HomeIcon } from "@heroicons/react/24/solid"
+
 const Error = () => {
+  const error = useRouteError() 
+  
+
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Oh dear we have a problem</h1>
+      <p>{error.message || error.status}</p>
+      <div className="flex-md">
+        <button></button>
+        <Link to="/" className="btn btn--dark">
+        <HomeIcon width={20} />
+        <span>Go home</span>
+        </Link>
+      </div>
     </div>
   )
 }
