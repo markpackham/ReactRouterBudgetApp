1ed0181c68bb8fddb66aea92a43e5c87d33434b9
mark <markpackham1@gmail.com>
2023-03-10 09:10:05 +0000

    add AddBudgetForm
    https://youtu.be/PbgVdQvhtqU?t=169

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,7 +13,8 @@ import { toast } from "react-toastify";
 // Loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName }
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
 }
 
 // Action
@@ -29,13 +30,21 @@ export async function dashboardAction({request}){
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName, budgets } = useLoaderData()
 
   return (
     <>
       {userName ? (
         <div className="dashboard">
           <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+          {/* {budgets ? () : ()} */}
+          <div className="grid-lg">
+            <div className="flex-lg">
+              <AddBudgetForm />
+            </div>
+          </div>
+          </div>
         </div>
       ) : <Intro /> }
     </>
