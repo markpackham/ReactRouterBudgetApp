8ef1dbc91d95501ebbd67ff755ff59842763afe0
mark <markpackham1@gmail.com>
2023-03-21 16:07:56 +0000

    add focus Ref
    https://youtu.be/awfA5UUhQHY?t=289

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,37 +1,61 @@
-import { Form, useFetcher } from "react-router-dom"
+// React
+import { useEffect, useRef } from "react";
+// RRD
+import { Form, useFetcher } from "react-router-dom";
 
 // Libraries
-import { CurrencyPoundIcon } from "@heroicons/react/24/solid"
+import { CurrencyPoundIcon } from "@heroicons/react/24/solid";
 
 const AddBudgetForm = () => {
-    const fetcher = useFetcher()
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === "submitting";
+
+  const formRef = useRef();
+  const focusRef = useRef();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      formRef.current.reset();
+      focusRef.current.focus();
+    }
+  }, [isSubmitting]);
+
   return (
     <div className="form-wrapper">
-        <h2 className="h3">
-            Create budget
-        </h2>
-        <Form method="post" className="grid-sm">
+      <h2 className="h3">Create budget</h2>
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
         <div className="grid-xs">
-            <label htmlFor="newBudget">Budget Name</label>
-            <input type="text" name="newBudget" id="newBudget" placeholder="e.g Groceries" required />
+          <label htmlFor="newBudget">Budget Name</label>
+          <input
+            type="text"
+            name="newBudget"
+            id="newBudget"
+            placeholder="e.g Groceries"
+            required
+            ref={focusRef}
+          />
         </div>
         <div className="grid-xs">
-            <label htmlFor="newBudgetAmount">Amount</label>
-            <input type="number" step="0.01"
-            name="newBudgetAmount" id="newBudgetAmount" placeholder="e.g £50"
+          <label htmlFor="newBudgetAmount">Amount</label>
+          <input
+            type="number"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="e.g £50"
             required
             // Decimal mode is handy for mobiles since it offers the "." symbol when typing
             inputMode="decimal"
-            />
+          />
         </div>
-        <input type="hidden" name="_action" value="createBudget"/>
+        <input type="hidden" name="_action" value="createBudget" />
         <button type="submit" className="btn btn--dark">
-            <span>Create Budget</span>
-            <CurrencyPoundIcon width={20} />
+          <span>Create Budget</span>
+          <CurrencyPoundIcon width={20} />
         </button>
-        </Form>
+      </fetcher.Form>
     </div>
-  )
-}
+  );
+};
 
-export default AddBudgetForm
+export default AddBudgetForm;
