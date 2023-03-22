d74209a65ecf35ac58eac1b2981c01d30748865d
mark <markpackham1@gmail.com>
2023-03-22 10:38:56 +0000

    Add Expense Form
    https://youtu.be/qre4LgXyTPo?t=279

---
+++ src/components/AddExpenseForm.jsx
@@ -0,0 +1,9 @@
+const AddExpenseForm = () => {
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">Add New</h2>
+    </div>
+  );
+};
+
+export default AddExpenseForm;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useLoaderData } from "react-router-dom";
 // Components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
 
 //  Helper functions
 import { createBudget, fetchData, waaaait } from "../helpers";
@@ -59,12 +60,20 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {/* {budgets ? () : ()} */}
-            <div className="grid-lg">
-              <div className="flex-lg">
-                <AddBudgetForm />
+            {bugets && bugets.length > 0 ? (
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal bugeting is the secret to financial freedom</p>
+                <p>Create a budget to get started</p>
+                <AddBudgetForm />
+                <AddExpenseForm budgets={budgets} />
+              </div>
+            )}
           </div>
         </div>
       ) : (
