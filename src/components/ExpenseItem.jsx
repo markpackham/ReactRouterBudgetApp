7e0f64446a614acd358d420bd343ca996df3215c
mark <markpackham1@gmail.com>
2023-03-27 16:36:33 +0100

    add colors for budget links
    https://youtu.be/m_7YL9W1KuY?t=386

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,4 +1,6 @@
 // helper imports
+import { Link } from "react-router-dom";
+
 import { formatCurrency, formatDateToLocaleString } from "../helpers";
 
 const ExpenseItem = ({ expense }) => {
@@ -6,13 +8,17 @@ const ExpenseItem = ({ expense }) => {
     category: "budgets",
     key: "id",
     value: expense.budgetId,
-  });
+  })[0];
   return (
     <>
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
       <td>{formatDateToLocaleString(expense.createdAt)}</td>
-      <td>{formatDateToLocaleString(expense.createdAt)}</td>
+      <td>
+        <Link to={`/budget/${budget.id}`} style={{ "--accent": budget.color }}>
+          {budget.name}
+        </Link>
+      </td>
     </>
   );
 };
