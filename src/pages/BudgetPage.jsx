8f0d87829fa72fa4b96eb71e1cefec35f3d8ad9c
mark <markpackham1@gmail.com>
2023-03-28 11:00:03 +0100

    throw error if budget doesn't exist
    https://youtu.be/jIBuvmxI9jQ?t=277

--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -12,11 +12,15 @@ export async function budgetLoader({ params }) {
     value: params.id,
   })[0];
 
+  if (!budget) {
+    throw new Error("The budget you're trying to find doesn't exist");
+  }
+
   return { budget };
 }
 
 const BudgetPage = () => {
   const { budget } = useLoaderData();
-  return <div>BudgetPage</div>;
+  return <div>{budget.name}</div>;
 };
 export default BudgetPage;
