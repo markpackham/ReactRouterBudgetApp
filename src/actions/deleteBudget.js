599d508a355641f8b721c5706e21457e55d9c8cc
mark <markpackham1@gmail.com>
2023-03-28 13:51:52 +0100

    delete associated expenses

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -1,8 +1,11 @@
+// rrd imports
+import { redirect } from "react-router-dom";
+
 // libraries
 import { toast } from "react-toastify";
 
 // helpers
-import { deleteItem } from "../helpers";
+import { deleteItem, getAllMatchingItems } from "../helpers";
 
 export function deleteBudget({ params }) {
   try {
@@ -11,8 +14,23 @@ export function deleteBudget({ params }) {
       id: params.id,
     });
 
+    const associatedExpenses = getAllMatchingItems({
+      category: "expenses",
+      key: "budgetId",
+      value: params.id,
+    });
+
+    associatedExpenses.array.forEach((expense) => {
+      deleteItem({
+        key: "expenses",
+        id: expense.id,
+      });
+    });
+
     toast.success("Budget deleted successfully");
   } catch (error) {
     throw new Error("There was a problem deleting the budget");
   }
+
+  return redirect("/");
 }
