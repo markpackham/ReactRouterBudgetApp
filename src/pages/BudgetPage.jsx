6bfd5bbc60d830c8c5b28dda538fc21ee4167dcd
mark <markpackham1@gmail.com>
2023-03-28 10:36:37 +0100

    add bugetLoader function
    https://youtu.be/jIBuvmxI9jQ?t=190

--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -1,3 +1,14 @@
+// loader
+export async function budgetsLoader({ params }) {
+  const budget = await getAllMatchingItems({
+    cateogry: "budgets",
+    key: "id",
+    value: params.id,
+  })[0];
+
+  return { budget };
+}
+
 const BudgetPage = () => {
   return <div>BudgetPage</div>;
 };
