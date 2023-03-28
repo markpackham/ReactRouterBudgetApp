4a17ca8cf5c026784f46b5e784db296ec4873566
mark <markpackham1@gmail.com>
2023-03-28 12:31:44 +0100

    allow deletion in BugetItem

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,10 +1,17 @@
+// rrd imports
+import { Form, Link } from "react-router-dom";
+
+// library imports
+import { BanknotesIcon, TrashIcon } from "@heroicons/react/24/outline";
+
+// helper functions
 import {
   calculateSpentByBudget,
   formatCurrency,
   formatPercentage,
 } from "../helpers";
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
 
@@ -26,6 +33,35 @@ const BudgetItem = ({ budget }) => {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)} remaining</small>
       </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="post"
+            action="delete"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Are you sure you want to permanently delete this budget?"
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit" className="btn">
+              <span>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
