a73267090af47ad58f62bdc15abda53f33539a83
mark <markpackham1@gmail.com>
2023-03-28 13:26:11 +0100

    throw item deletion in a try catch
    https://youtu.be/aALLPIGgOQE?t=560

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -1,1 +1,12 @@
-export function deleteBudget() {}
+import { deleteItem } from "../helpers";
+
+export function deleteBudget({ params }) {
+  try {
+    deleteItem({
+      key: "budgets",
+      id: params.id,
+    });
+  } catch (error) {
+    throw new Error("There was a problem deleting the budget");
+  }
+}
